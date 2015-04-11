0e00b03d420b37d1ea0d6f5771140cf173c0fa8c
Alex Manyulenko <snek-kot1996@mail.ru>
2015-04-11 15:37:20 +0300

    change url

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 <body>
 <div class="text-center">
     <br>
-    <img src="img\Test_img.png" class="img-circle">
+    <img src="img/Test_img.png" class="img-circle">
     <br>
     <br>
     <br>
-    <form id="password" action="../main.php" method="POST">
+    <form id="password" action="main.php" method="POST">
         <input type="password" name="pw" id="textfield" maxlength="21"><br>
         <br><br>
         <input type="image" src="img/enter_button.png" onmouseover="this.src='img/enter_button_hover.png'" onmouseout="this.src='img/enter_button.png'" alt="Enter">

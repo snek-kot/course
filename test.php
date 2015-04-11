fa542cb25b54839e544a11748af9f88574c5cccf
VladM <pleo12@gmail.com>
2015-04-11 18:38:06 +0300

    fonts

--- test.php
+++ test.php
@@ -1,23 +1,19 @@
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Course</title>
-    <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
-    <link href="css/test.css" rel="stylesheet">
 </head>
 <body>
-
 <div class="navbar navbar-default navbar-static-top navbar-inverse">
     <div class="navbar-header">
         <a href="#" class="navbar-brand">tests</a>
+        <span class="glyphicon glyphicon-search" aria-hidden="true">Test</span>
     </div>
     <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Test</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-search" aria-hidden="true">Test</span></a></li>
         <li><a href="#">Profile</a></li>
         <li class="divider-vertical"></li>
         <li><a href="#">About</a></li>
@@ -26,9 +22,20 @@
 </div>
 
 <div class="row">
-    <div class="col-md-6" style="border-style: solid 1px;">
+    <div class="col-md-5 col-md-offset-2">
         <img src="http://placehold.it/500x500" class="img-rounded">
     </div>
+    <div class="col-md-5">
+        <p>Envelope icon: <span class="glyphicon glyphicon-inbox"></span></p> 
+        <p>Inbox icon on a styled link button:
+        <a href="#" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-inbox"></span> Inbox
+        </a>
+      </p> 
+    </div>
 </div>
+
+<script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>

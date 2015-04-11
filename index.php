2cdfa776d600acb6e37ef0715f69d2f9edecdff5
Alex Manyulenko <snek-kot1996@mail.ru>
2015-04-11 14:33:11 +0300

    initial

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Course</title>
+    <script src="js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+</head>
+<body>
+<div class="text-center">
+    <br>
+    <img src="img\Test_img.png" class="img-circle">
+    <br>
+    <br>
+    <br>
+    <form id="password" action="../main.php" method="POST">
+        <input type="password" name="pw" id="textfield" maxlength="21"><br>
+        <br><br>
+        <input type="image" src="img/enter_button.png" onmouseover="this.src='img/enter_button_hover.png'" onmouseout="this.src='img/enter_button.png'" alt="Enter">
+    </form>
+</div>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Course</title>
+    <script src="js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+</head>
+<body>
+    <nav id="navbar" class="navbar navbar-fixed-top">
+        <div class="btn-group">
+            <button type="button" class="btn btn-danger">Action</button>
+            <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                <span class="caret"></span>
+                <span class="sr-only">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+            </ul>
+        </div>
+    </nav>
+
+</body>
+</html>
